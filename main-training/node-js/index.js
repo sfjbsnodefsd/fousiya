670de2b5bc259910ce88cf113002cd8a6f91ca60
sfjbssept <sfjbssept@gmail.com>
2022-09-20 15:43:47 +0000

    Json program

--- main-training/node-js/index.js
+++ main-training/node-js/index.js
@@ -1,9 +1,37 @@
 //console.log("Hello this is Nishant, I hope you are having a great day! ");
-const http = require('http');
+const http = require('http')  
 
-    function greet(req,res) {
-    res.write("<h1>Hello this is Fousya, I hope you are having a great day!<h1> ")
-    res.end();
+
+
+function greet(req,res){
+
+    res.writeHead(200,{'Content-Type':'application/json'});
+
+    res.write(JSON.stringify({
+
+        "name":"nishant",
+
+        "Empid":"001",
+
+
+
+        "address":{
+
+            "street":"xyz street",
+
+            "city":"Bangalore",
+
+            "State":"Karnataka"
+
+        }
+
+    }))
+
+    res.end()
 
 }
-http.createServer(greet).listen(5000);
+
+ 
+
+http.createServer(greet) .listen(5000);
+
