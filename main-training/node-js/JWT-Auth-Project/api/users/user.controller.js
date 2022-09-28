40a8d9d37d72d799336c8ca4581be4a29a4d109f
sfjbssept <sfjbssept@gmail.com>
2022-09-28 16:07:59 +0000

    code completed for CRUDcreate, deleteUser, getUsers, getUserById, updateUser

--- main-training/node-js/JWT-Auth-Project/api/users/user.controller.js
+++ main-training/node-js/JWT-Auth-Project/api/users/user.controller.js
@@ -1,6 +1,7 @@
 const { hashSync } = require("bcrypt");
-const {create} = require("./user.service")
-const {genSaltSync} = require("bcrypt");
+const { create, deleteUser, getUsers, getUserById, updateUser } = require("./user.service");
+
+const { genSaltSync } = require("bcrypt");
 
 
 
@@ -27,6 +28,69 @@ module.exports = {
             });
         });
     },
+    getUserById: (req, res) => {
+        const id = req.params.id;
+        getUserById(id, (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            } if (!results) {
+                return res.json({
+                    success: 0,
+                    message: "Record not found!"
+                })
+            } return res.json({
+                success: 1,
+                data: results
+            })
+        })
+    },
 
+    getUsers : (req, res) => {
+        getUsers((err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success: 1,
+            });
+        });
 
+    },
+        updateUser: (req, res) => {
+            const body = req.body;
+            const salt = genSaltSync(10);
+            body.password = hashSync(body.password, salt);
+            updateUser(body, (err, results) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                if(!results){
+                    res.json({
+                        success:0,
+                        message:"failed to operate user"
+
+                    })
+                }
+                return res.json({
+                    success: 1,
+                    message: "Updated successfully"
+                })
+            })
+
+        },
+        deleteUser: (req, res) => {
+            const data = req.body;
+            deleteUser(data, (err) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                } return res.json({
+                    success: 1,
+                    message: "User deleted successfully"
+                })
+            })
+        },
 };
--- main-training/node-js/JWT-Auth-Project/api/users/user.service.js
+++ main-training/node-js/JWT-Auth-Project/api/users/user.service.js
@@ -4,10 +4,10 @@ const pool = require("../../config/database")
 
 
 module.exports = {
-   //if we get error it will be passed as data and callback will be null
+    //if we get error it will be passed as data and callback will be null
     //if execution is successful the callback will have data
 
-    create:(data,callback) => {
+    create: (data, callback) => {
         pool.query(
             `insert into registration (firstName,lastName,gender,email,password,number)
             values(?,?,?,?,?,?)`,
@@ -20,15 +20,98 @@ module.exports = {
                 data.number,
 
             ],
-            (error,results, fields) => {
-if(error){
-    return callback(error);
-    }
-    return callback(null,results);
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, results);
 
             }
         );
     },
+    getUsers: (callBack) => {
+
+        pool.query(
+    
+          //`select id,firstName,lastName,gender,email,number from registration`,
+          `select * from registration`,
+    
+          [],
+    
+          (error, results, fields) => {
+    
+            if (error) {
+    
+              return callBack(error);
+    
+            }
+    
+            return callBack(null, results);
+    
+          }
+    
+        );
+    
+      },
+    
+      getUserById: (id,callBack) => {
+    
+        pool.query(
+    
+          `select id,firstName,lastName,gender,email,number from registration where id = ?`,
+    
+          [id],
+    
+          (error, results, fields) => {
+    
+            if (error) {
+    
+              return callBack(error);
+    
+            }
+    
+            return callBack(null, results);
+    
+          }
+    
+        );
+    
+      },
+updateUser: (data, callback) => {
+    pool.query(
+        `update registration set firstName =?, lastName =?, email =? password=?,number=? where id = ?`,
+        [
+            data.first_Name,
+            data.last_name,
+            data.gender,
+            data.email,
+            data.password,
+            data.number
+        ], (error, results, fields) => {
+            if (error) {
+                callback(error);
+            }
+            return callback(null, results)
+        }
+
+    )
+},
+
+deleteUser: (data, callback) => {
+    pool.query(
+        `delete from registration where id = ?`,
+        [data.id],
+        (error, results, fields) => {
+            if (error) {
+                callback(error);
+                return callback(error);
+            } return callback(null, results[0]);
+        }
+    )
+},
+
+
 };
+
 
 
