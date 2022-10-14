6198080c0839b7a5a1cc023b926a748c89affc09
sfjbssept <sfjbssept@gmail.com>
2022-10-14 12:20:48 +0000

    subfunctions created for unit testing

--- main-training/node-js/Pension-Management-System/process-pension/index.js
+++ main-training/node-js/Pension-Management-System/process-pension/index.js
@@ -15,17 +15,105 @@ app.post("/ProcessPension", async (req, res) => {
 
 
     //get pension details
-    const url = `${endpoint}/getPensionerDetailByAadhaar/${aadhaar}`
-    request.get(url, { json: true }, (err, result, body) => {
-        if (err) {
+    // const url = `${endpoint}/getPensionerDetailByAadhaar/${aadhaar}`
+    // request.get(url, { json: true }, (err, result, body) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+
+
+    //     const { SalaryEarned, Allowances, SelfOrFamilyPension, BankDetails } = body;
+
+
+    //     var percentage = null;
+    //     if (SelfOrFamilyPension !== null) {
+    //         switch (SelfOrFamilyPension.toString().toUpperCase()) {
+    //             case "SELF":
+    //                 percentage = .80;
+    //                 break;
+    //             case "FAMILY":
+    //                 percentage = .50;
+    //                 break;
+
+    //         }
+    //     }
+
+    //     const _pensionAmount = (percentage * SalaryEarned) + Allowances;
+
+    //     let serviceCharge;
+    //     if (BankDetails) {
+    //         switch (BankDetails.PublicOrPrivateBank.toString().toUpperCase()) {
+    //             case "PRIVATE":
+    //                 serviceCharge = 550;
+    //                 break;
+    //             case "PUBLIC":
+    //                 serviceCharge = 500;
+    //                 break;
+
+
+    //         }
+
+    //     }
+    
+
+    //     return result
+    //     res.status(200).json( {
+    //         pensionAmount: _pensionAmount,
+    //         bankServiceCharge: serviceCharge
+    //     });
+
+    // })
+    try {
+        const pensioner = await getPensionDetails(aadhaar);
+        const { SalaryEarned, Allowances, SelfOrFamilyPension, BankDetails } = pensioner;
+        const percentage = getPercentage(SelfOrFamilyPension);
+
+        const _pensionAmount = (percentage * SalaryEarned) + Allowances;
+        const _serviceCharge = getServiceCharge(BankDetails);
+        //return result
+        res.status(200).json({
+            pensionAmount: _pensionAmount,
+            bankServiceCharge: _serviceCharge
+        });
+
+
+
+    } catch (err) {
+        throw err;
+    }
+
+});
+
+
+
+const getPensionDetails = (aadhaar) => {
+
+    return new Promise((resolve,reject)=>{
+        try {
+           
+            const url = `http://localhost:5001/getPensionerDetailByAadhaar/${aadhaar}`
+            request.get(url, { json: true }, (err, result, body) => {
+                if (err) {
+                    console.log(err);
+                    reject(err) ;
+                }
+                else
+                    resolve(body) ;
+            });
+        } catch (err) {
             console.log(err);
-            return;
+            reject(err) ;
         }
+    
+    });
+    
+       
+    
+    
+    }
 
-
-        const { SalaryEarned, Allowances, SelfOrFamilyPension, BankDetails } = body;
-
-
+    const getPercentage = (SelfOrFamilyPension) => {
         var percentage = null;
         if (SelfOrFamilyPension !== null) {
             switch (SelfOrFamilyPension.toString().toUpperCase()) {
@@ -35,12 +123,13 @@ app.post("/ProcessPension", async (req, res) => {
                 case "FAMILY":
                     percentage = .50;
                     break;
-
+    
             }
         }
-
-        const _pensionAmount = (percentage * SalaryEarned) + Allowances;
-
+        return percentage;
+    }
+    
+    const getServiceCharge = (BankDetails) => {
         let serviceCharge;
         if (BankDetails) {
             switch (BankDetails.PublicOrPrivateBank.toString().toUpperCase()) {
@@ -50,22 +139,13 @@ app.post("/ProcessPension", async (req, res) => {
                 case "PUBLIC":
                     serviceCharge = 500;
                     break;
-
-
+    
+    
             }
-
+    
         }
-
-        //return result
-        res.status(200).json( {
-            pensionAmount: _pensionAmount,
-            bankServiceCharge: serviceCharge
-        });
-
-    })
-
-});
-
+        return serviceCharge;
+    }
 
 
 
