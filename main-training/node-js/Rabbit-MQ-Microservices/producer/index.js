68381221d2f49b3710eca680495bcf5f1dc9307b
sfjbssept <sfjbssept@gmail.com>
2022-10-21 14:04:02 +0000

    added producer service

---
+++ main-training/node-js/Rabbit-MQ-Microservices/producer/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const amqp = require("amqplib");
+var channel, connection;
+async function connect() {
+  try {
+    const amqpServer = "amqp://localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("Rabbit");
+  } catch (err) {
+    console.log(err);
+  }
+}
+connect();
+
+
+app.get("/send",async (req, res) => {
+  
+});
+app.listen(5001, () => {
+  console.log("server is running at port 5000");
+});
