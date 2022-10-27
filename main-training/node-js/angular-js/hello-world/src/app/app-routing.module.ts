217a25606abdd799feeb8dc58f6989c1bf43e8dc
sfjbssept <sfjbssept@gmail.com>
2022-10-27 14:29:46 +0000

    intregrated bootstrap and added nav bar

--- main-training/node-js/angular-js/hello-world/src/app/app-routing.module.ts
+++ main-training/node-js/angular-js/hello-world/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+import { UserFormComponent } from './Components/user-form/user-form.component';
+
+const routes: Routes = [{path :"", component:HomeComponent}, {path:"register", component:UserFormComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
