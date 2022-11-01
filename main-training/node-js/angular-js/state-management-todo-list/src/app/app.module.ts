8c657639387777cde2cbb21cc84b9da5aa24fffa
sfjbssept <sfjbssept@gmail.com>
2022-11-01 15:27:03 +0000

    added both components and set up the html and the method

--- main-training/node-js/angular-js/state-management-todo-list/src/app/app.module.ts
+++ main-training/node-js/angular-js/state-management-todo-list/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoReducer } from './Reducer/todo.reducer';
+import { AddTodoComponent } from './Components/add-todo/add-todo.component';
+import { DisplayTodoComponent } from './Components/display-todo/display-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    DisplayTodoComponent
   ],
   imports: [
     BrowserModule,
