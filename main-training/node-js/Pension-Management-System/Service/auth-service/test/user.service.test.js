0ee1e8f4e6a8a052d6be26d359239e4abaee4e7c
sfjbssept <sfjbssept@gmail.com>
2022-11-06 14:09:27 +0000

    dependency added

---
+++ main-training/node-js/Pension-Management-System/Service/auth-service/test/user.service.test.js
@@ -0,0 +1,14 @@
+// // Requiring module
+// const assert = require('assert');
+// const {create,getUserByUserEmail} = require('../service/user.service')
+// const User = require("../models/user");
+
+// module.exports = async it => {
+     
+ 
+//     it("User Registration Test", () => eq(1 + 1, 2));
+ 
+//     it("Get User By Email Test", () => eq(1 + 2, 3));
+ 
+//     it("fib 03", () => eq(2 + 3, 5));
+// };
