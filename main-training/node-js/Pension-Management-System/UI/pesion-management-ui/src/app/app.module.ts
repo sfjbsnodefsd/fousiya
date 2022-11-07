d619161ee2ccf321e0238f5ef22d126fb1c12a1c
sfjbssept <sfjbssept@gmail.com>
2022-11-07 11:01:19 +0000

    UI components created

--- main-training/node-js/Pension-Management-System/UI/pesion-management-ui/src/app/app.module.ts
+++ main-training/node-js/Pension-Management-System/UI/pesion-management-ui/src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PensionerDetailComponent } from './components/pensioner-detail/pensioner-detail.component';
+import { ProcessPensionComponent } from './components/process-pension/process-pension.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginComponent
+    UserLoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    PensionerDetailComponent,
+    ProcessPensionComponent,
+   
   ],
   imports: [
     BrowserModule,
---
+++ main-training/node-js/Pension-Management-System/UI/pesion-management-ui/src/app/components/pensioner-detail/pensioner-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensioner-detail',
+  templateUrl: './pensioner-detail.component.html',
+  styleUrls: ['./pensioner-detail.component.css']
+})
+export class PensionerDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
